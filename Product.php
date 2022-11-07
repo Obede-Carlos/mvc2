e5d7c6fd9b3c7a9be75ee45c272330f7c90c2f60
Carlos Obedé <carlosobede@gmail.com>
2022-11-07 17:31:09 +0100

    mvc00

---
+++ Product.php
@@ -0,0 +1,27 @@
+<?php 
+    //Fichero que simula el modelo de datos.
+    class Product{
+        const PRODUCTS = [
+            array(1, 'Contracesped'),
+            array(2, 'Pizarra'),
+            array(3, 'Billar'),
+            array(4, 'Dados'),
+        ];
+
+        function __construct()
+        {
+            //constructor vacio.
+        }
+
+        //devuelve todos los productos.
+        public static function all()
+        {
+            return Product::PRODUCTS;
+        }
+
+        //devolver un producto en particular.
+        public function find($id)
+        {
+            return Product::PRODUCTS[$id-1];
+        }
+    }
